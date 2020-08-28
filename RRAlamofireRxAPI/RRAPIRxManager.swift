b8dc0d28cf344507adb2280e969093274d204fab
Rahul Mayani <mayani.rahul@gmail.com>
2020-08-28 14:35:59 +0530

    Update RRAPIRxManager.swift

--- RRAlamofireRxAPI/RRAPIRxManager.swift
+++ RRAlamofireRxAPI/RRAPIRxManager.swift
@@ -106,11 +106,30 @@ extension RRAPIRxManager {
 
 extension ObservableType {
     
+    /// Get observable
+    /// The Defer operator waits until an observer subscribes to it, and then it generates an Observable,
+    /// typically with an Observable factory function. It does this afresh for each subscriber, so although each subscriber may think it is subscribing to the same Observable,
+    /// in fact each subscriber gets its own individual sequence.
+    /// Default implementation of converting `ObservableType` to `Observable`.
+    public func setDeferredAsObservable() -> Observable<Element> {
+        return Observable.deferred {
+            return self.asObservable()
+        }
+    }
+    
     /**
      Makes the observable Subscribe to concurrent background thread and Observe on main thread
      */
     public func subscribeConcurrentBackgroundToMainThreads() -> Observable<Element> {
         return self.subscribeOn(RXScheduler.concurrentBackground)
+            .observeOn(RXScheduler.main)
+    }
+    
+    /**
+     Makes the observable Subscribe to concurrent background thread with delay and Observe on main thread
+     */
+    public func delaySubscribeConcurrentBackgroundToMainThreads(_ time: RxTimeInterval = .seconds(2)) -> Observable<Element> {
+        return self.delaySubscription(time, scheduler: RXScheduler.concurrentBackground)
             .observeOn(RXScheduler.main)
     }
 }
