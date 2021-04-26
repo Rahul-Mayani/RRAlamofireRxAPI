5f343ad6da24f71a0a1bc392840b5c6bbedd4b81
Rahul <mayani.rahul@gmail.com>
2021-04-26 11:14:49 +0530

    resolved SPM support issue

--- Package.swift
+++ Package.swift
@@ -15,16 +15,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Rahul-Mayani/RRAlamofireRxAPI", from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.0.0")),
-        .package(url: "https://github.com/ReactiveX/RxCocoa.git", .exact("6.0.0")),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", . exact("4.9.1"))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RRAlamofireRxAPI",
-            dependencies: ["RxSwift", "RxCocoa", "Alamofire"]),
+            dependencies: ["RxSwift", "Alamofire"]),
         .testTarget(
             name: "RRAlamofireRxAPITests",
             dependencies: ["RRAlamofireRxAPI"])
